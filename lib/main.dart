1df02ab5f5496cd058b0c2cf308d9105dbb76f5d
KerubinDev <kelvin.moraes117@gmail.com>
2025-01-23 23:28:04 -0300

    finalização do app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ import 'providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "YOUR_API_KEY",
+      authDomain: "YOUR_AUTH_DOMAIN",
+      projectId: "YOUR_PROJECT_ID",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+      appId: "YOUR_APP_ID",
+    ),
+  );
   final prefs = await SharedPreferences.getInstance();
   
   runApp(MyApp(prefs: prefs));
