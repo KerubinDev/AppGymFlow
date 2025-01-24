db5afdfc5f0d0c544a8bca54aca30da80010151e
KerubinDev <kelvin.moraes117@gmail.com>
2025-01-24 13:51:15 -0300

    finalização do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/workout_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FitTracker',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => WorkoutProvider(),
+      child: MaterialApp(
+        title: 'GymFlow',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const WelcomeScreen(),
-      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GymFlow'),
+      ),
+      body: const Center(
+        child: Text('Bem-vindo ao GymFlow!'),
+      ),
     );
   }
 } 
